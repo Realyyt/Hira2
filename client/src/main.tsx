7da05771ac9d9fdf14cdd01044984c4c54ee4403
realyyt <emmyisaac60@gmail.com>
2025-07-02 17:08:10 +0100

    feat

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n/config";
 import "./index.css";
-
+//s
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
